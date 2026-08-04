4e7b7076f02746b3e5dc3ad80add4dd08700c4ef
nikeishaumeka <007keisha@gmail.com>
2026-08-04 13:39:30 +0700

    Initial commit

---
+++ HTML_Dasar/form_html.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+    <form>
+        Nama: <input type="text" name="nama"><br/>
+        Alamat: <input type="text" name="alamat"><br/>
+        Password: <input type="password" name="pass"><br/>
+        jenis kelamin :
+                <input type="radio" name="jk" value="laki-laki"/> Laki-Laki
+                 <input type="radio" name="jk" value="perempuan"/> Perempuan
+                 <br/>
+        bahas pemrograman yang dikuasai :
+                <input type="checkbox" nama="web"> Web
+                <input type="checkbox" nama="mobile"> MOBILE 
+                <input type="checkbox" nama="desk"> DESKTOP 
+                <br/>
+        kota asal :
+            <select>
+                <option>Semarang</option>
+                <option>Bandung</option>
+            </select>
+        <br/>
+        pesan anda :
+        <textarea></textarea>
+        <br/>
+        <input type="sumbit" value="proses">
+    </form>
+</body>
+</html>
